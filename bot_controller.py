023df525c60141c0f4685c54f56ed56fa8383b85
groverari <41345908+groverari@users.noreply.github.com>
2022-03-12 03:08:18 -0800

    Created bot.py

---
+++ bot_controller.py
@@ -0,0 +1,14 @@
+
+class BotController:
+
+    def __init__(self):
+        pass
+
+    def add_stock(self, stock_name):
+        pass
+
+    def push_stock(self):
+        pass
+
+    def watch_stocks(self):
+        pass
