d5198ddf1f0647e7bd1300e680013f43513123ab
groverari <41345908+groverari@users.noreply.github.com>
2022-03-14 23:39:45 -0700

    Testing strategies in bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,35 @@
 import discord
 import os
 from dotenv import load_dotenv
+import bot_controller
+from bot_controller import *
 
-load_dotenv()
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+class bot:
 
-    if message.content.startswith('$add'):
-        await message.channel.send(f"{message.author.mention} hello")
+    def __init__(self):
+        load_dotenv()
+        self.client = discord.Client()
+        self.client.run(os.getenv('TOKEN'))
 
-client.run(os.getenv('TOKEN'))
+    @client.event
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self.client))
+
+
+    @client.event
+    async def on_message(self, message):
+        if message.author == self.client.user:
+            return
+
+        if message.content.startswith('$add '):
+            await message.channel.send("hello")
+            #BotController.add_stock(message.author.id, message.content)
+
+
+    def send_message(username, message):
+        message.channel.send(f"<@{username} hello")
+
+
+
--- bot_controller.py
+++ bot_controller.py
@@ -1,9 +1,12 @@
+import bot
+from bot import *
+
 
 class BotController:
-
-
+    @staticmethod
     def add_stock(username, stock_name):
-        pass
+        print("bot control")
+        bot.send_message(username, "hello")
 
     def push_stock(self):
         pass
